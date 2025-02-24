819f6085de0dafe3b6892960bda90d8687b21cdd
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-02-24 14:05:36 +0530

    Create promise-await.js
    The await keyword is used to wait for a Promise to resolve.

---
+++ Promise/promise-await.js
@@ -0,0 +1,8 @@
+// The await keyword is used to wait for a promise to resolve. 
+// Once it has been resolved, the await expression returns the value of the resolved Promise.
+
+const message = await promise;
+console.log(`Resolved with ${message}`);
+
+// References
+// https://www.freecodecamp.org/news/http-full-course/
