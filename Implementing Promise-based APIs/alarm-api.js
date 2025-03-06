b10ed7943cc83e55f21fc8e68e9ff250ad1decaa
thatperturbedprogrammer <96286692+thatperturbedprogrammer@users.noreply.github.com>
2025-03-06 11:15:56 +0530

    Create alarm-api.js

---
+++ Implementing Promise-based APIs/alarm-api.js
@@ -0,0 +1,28 @@
+const name = document.querySelector("#name");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#set-alarm");
+const output = document.querySelector("#output");
+
+function alarm(person, delay) {
+  return new Promise((resolve, reject) => {
+    if (delay < 0) {
+      reject(new Error("Alarm delay must not be negative"));
+      return;
+    }
+    setTimeout(() => {
+      resolve(`Wake up, ${person}!`);
+    }, delay);
+  });
+}
+
+button.addEventListener("click", async () => {
+  try {
+    const message = await alarm(name.value, delay.value);
+    output.textContent = message;
+  } catch (error) {
+    output.textContent = `Couldn't set alarm: ${error}`;
+  }
+});
+
+
+// Resources: https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Async_JS/Implementing_a_promise-based_API
